41331e9cf8848056c26b131d16472990aa12f45c
stepan41k <russia.zont@gmail.com>
2026-05-25 23:20:36 +0300

    feat(frontend): update tailwind config

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // ПРОВЕРЬ: должны быть указаны tsx и jsx
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
